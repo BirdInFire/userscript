efa94eff9c1af36586facf20f37f719aa5832284
Birdinfire <42673166+BirdInFire@users.noreply.github.com>
2023-06-13 12:49:14 +0200

    test
    Some test

--- redirect.js
+++ redirect.js
@@ -1,59 +1,37 @@
 // ==UserScript==
-// @name         Redirect Script
-// @namespace    your-namespace
-// @version      1.0
-// @description  Redirects specified URLs to different hosts with HTTPS scheme.
-// @match        *://reddit.com/*
-// @match        *://twitter.com/*
-// @match        *://youtube.com/*
-// @match        *://medium.com/*
-// @match        *://imdb.com/*
-// @match        *://imgur.com/*
-// @match        *://stackoverflow.com/*
-// @match        *://wikipedia.org/*
+// @name         Redirect to Perdu
+// @author       BirdInFire
+// @description  Redirection of my extension to safari with userscript
+// @version      1
+// @match        *://*.reddit.com/*
+// @match        *://*.twitter.com/*
+// @match        *://*.youtube.com/*
+// @match        *://*.medium.com/*
+// @match        *://*.imdb.com/*
+// @match        *://*.imgur.com/*
+// @match        *://*.stackoverflow.com/*
+// @match        *://*.wikipedia.org/*
 // @match        *://ajax.googleapis.com/*
 // @run-at       document-start
 // ==/UserScript==
 
 (function() {
-    'use strict';
+    function redirect() {
+        const host = location.hostname;
+        const path = location.pathname + location.search;
 
-    var redirects = [
-        { urlFilter: "||reddit.com", host: "libreddit.kutay.dev" },
-        { urlFilter: "||twitter.com", host: "twitter.dr460nf1r3.org" },
-        { urlFilter: "||youtube.com", host: "piped.hostux.net" },
-        { urlFilter: "||medium.com", host: "medium.hostux.net" },
-        { urlFilter: "||imdb.com", host: "lmdb.hostux.net" },
-        { urlFilter: "||imgur.com", host: "rimgo.hostux.net" },
-        { urlFilter: "||stackoverflow.com", host: "overflow.smnz.de" },
-        { urlFilter: "||wikipedia.org", host: "wikiless.tiekoetter.com" },
-        { urlFilter: "||ajax.googleapis.com", host: "cdnjs.cloudflare.com" }
-    ];
+        let redirectUrl = '';
 
-    function applyRedirect(url, host) {
-        var newUrl = url.replace(/^(https?:\/\/)([^/]+)/, function(match, scheme, oldHost) {
-            return scheme + host;
-        });
-        window.location.replace(newUrl);
-    }
-
-    function shouldApplyRedirect(url, condition) {
-        var urlFilter = condition.urlFilter;
-        var resourceTypes = condition.resourceTypes;
-
-        if (urlFilter === "*" && resourceTypes.includes("main_frame")) {
-            return true;
+        if (host.includes('youtube.com') || host.includes('www.youtube.com')) {
+            redirectUrl = 'https://piped.hostux.net';
+        } else if (host.includes('twitter.com')) {
+            redirectUrl = 'https://perdu.com';
         }
 
-        var urlRegex = urlFilter.replace(/[.?*+^$[\]\\(){}|-]/g, "\\$&").replace(/\\\*/g, ".*");
-        var regex = new RegExp(urlRegex, "i");
-
-        return regex.test(url) && resourceTypes.includes("main_frame");
+        if (redirectUrl !== '') {
+            location.href = redirectUrl + path;
+        }
     }
 
-    redirects.forEach(function(redirect) {
-        if (shouldApplyRedirect(window.location.href, redirect)) {
-            applyRedirect(window.location.href, redirect.host);
-        }
-    });
+    redirect();
 })();
